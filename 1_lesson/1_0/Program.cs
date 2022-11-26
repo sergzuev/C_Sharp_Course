f60ee73cd0e2a4c32ac3556e2fcfa14e56750142
Сергей Зуев <s.zuev@MacBook-Pro-Sergej.local>
2022-11-26 17:26:02 +0300

    Task 1_0

--- 1_lesson/1_0/Program.cs
+++ 1_lesson/1_0/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 0. Напишите программу, которая на вход принимает число и 
+//    выдает егот квадрат (число, умноженное на само себя) 
+
+Console.WriteLine("Write a number: ");
+
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine(num * num);
