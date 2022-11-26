91a60887760801f00e87ad2abf03997e477c547c
Сергей Зуев <s.zuev@MacBook-Pro-Sergej.local>
2022-11-26 19:12:02 +0300

    1_4 задача 1_4

---
+++ 1_lesson/1_4/Program.cs
@@ -0,0 +1,5 @@
+﻿// Напишите программу вычисления модуля числа
+Console.Write("Введите значение : ");
+var s = Console.ReadLine();
+int N = s == null ? 0 : int.Parse(s);
+Console.WriteLine($"{Math.Abs(N)}");
