d8432193b85d4b2b18c749c94bfef690c9e38546
Сергей Зуев <s.zuev@MacBook-Pro-Sergej.local>
2022-11-27 22:51:10 +0300

    home work 1_0

--- 1_lesson/home_work_1_0/Program.cs
+++ 1_lesson/home_work_1_0/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа
+// и выдаёт, какое число большее, а какое меньшее.
+
+ Console.WriteLine("Введите первое число: ");
+ string num1 = Console.ReadLine();
+ int a = int.Parse(num1);
+
+ Console.WriteLine("Введите второе число : ");
+ string num2 = Console.ReadLine();
+ int b = int.Parse(num2);
+
+ if(a > b)
+ {
+     Console.WriteLine($"Max = {a}");
+     Console.Write($"Min = {b}");
+ }
+ else if(a < b)
+ {
+     Console.WriteLine($"Max = {b}");
+     Console.Write($"Min = {a}");
+ }
+ else
+ {
+    Console.Write("Числа равны. Невозможно определить Max и Min"); 
+ }
