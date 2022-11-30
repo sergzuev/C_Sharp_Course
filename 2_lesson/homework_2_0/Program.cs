38cc1a4cd8f5f27132e51986afab6f5022bf18d2
Сергей Зуев <s.zuev@MacBook-Pro-Sergej.local>
2022-12-01 00:02:36 +0300

    Homework_2_0

---
+++ 2_lesson/homework_2_0/Program.cs
@@ -0,0 +1,12 @@
+﻿// напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите трёхзначное число: ");
+string NumSecond(int num)
+{
+    if ((-1000 < num && num < -100) || (num <1000 && num > 100))
+       return $"{num / 10 % 10}";
+    return "Число не является трёхзначным";
+}
+
+Console.WriteLine(NumSecond(int.Parse(Console.ReadLine())));
