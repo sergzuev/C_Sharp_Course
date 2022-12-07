59d0b3b39bc2d60e10a7dc9a91b8e751cd07e4f5
Сергей Зуев <s.zuev@MacBook-Pro-Sergej.local>
2022-12-07 21:08:14 +0300

    Task 4.1_1

---
+++ 4_lesson/4_1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число N и
+// выдает количество цифр в числе.
+// 456 -> 3; 78 -> 2; 89126 -> 5 .
+
+int NumbCount(int n)
+
+{
+    int count = 0;
+    if (n == 0)
+    {
+        return 1;
+    }
+    while (n > 0)
+    {
+        n = n / 10;
+        count++;
+    }
+    return count;
+}
+
+Console.WriteLine(NumbCount(int.Parse(Console.ReadLine())));
+
