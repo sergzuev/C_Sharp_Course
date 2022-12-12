c40dd668e877a14cb207d8e5bdc2127e7ad1ff21
Сергей Зуев <s.zuev@MacBook-Pro-Sergej.local>
2022-12-12 19:48:21 +0300

    homework 5_0

--- 5_lesson/5_3/Program.cs
+++ 5_lesson/5_3/Program.cs
@@ -1,23 +1,7 @@
 ﻿// Задайте одномерный массив из 123 случайных чисел.
 // Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
 
-void Print(int[] arr)
-{
-    int size = arr.Length;
-
-    for (int i = 0; i < size; i++)
-       Console.Write($"{arr[i]} ");
-    Console.WriteLine();
-}
-
-int[] ArrayNums(int size, int start, int last)
-{
-    int[] arr = new int[size];
-
-    for (int i = 0; i < size; i++)
-       arr[i] = new Random().Next(start, last + 1);
-    return arr;
-}
+c
 int CountDiap(int[] arr)
 {
     int count = 0;
