a89cf0db5e5930dfba527566c97dba4060b10601
Сергей Зуев <s.zuev@MacBook-Pro-Sergej.local>
2022-12-18 23:26:02 +0300

    Task 7_3

---
+++ 7_lesson/7_3/Program.cs
@@ -0,0 +1,52 @@
+﻿// 3. Задайте двумерный массив. Найдите сумму элементов главной диагонали.
+
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+            Console.Write($" {arr[i, j]} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+
+    return arr;
+}
+
+int SumDiagonal(int[,] arr)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+    int diag_sum = 0;
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            if (i == j)
+                diag_sum += arr[i, j];
+
+    return diag_sum;
+}
+
+Console.Write("Enter the number of rows: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Enter the number of columns: ");
+int column = int.Parse(Console.ReadLine());
+
+int[,] arr_1 = MassNums(row, column,
+                        int.Parse(Console.ReadLine()),
+                        int.Parse(Console.ReadLine()));
+Print(arr_1);
+
+Console.Write(SumDiagonal(arr_1));
