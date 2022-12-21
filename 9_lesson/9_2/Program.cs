c20c0d59da8df2df5ff634c6f0d1069564af5a47
Сергей Зуев <s.zuev@MacBook-Pro-Sergej.local>
2022-12-21 20:17:25 +0300

    Task 9_2

---
+++ 9_lesson/9_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая будет принимать на вход число
+// и возвращать сумму его цифр. Использовать рекурсию.
+
+
+int SumNum(int num)
+{
+    if (num == 0) return 0;
+    return SumNum(num / 10) + num % 10;    
+}
+
+Console.WriteLine(SumNum(453));
+Console.WriteLine(SumNum(45));
+
