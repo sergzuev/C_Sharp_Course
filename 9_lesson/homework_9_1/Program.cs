3e206689ad93ece29f3acbea00b7a673f8f6af29
Сергей Зуев <s.zuev@MacBook-Pro-Sergej.local>
2022-12-26 19:07:19 +0300

    Homework 9_1

--- 9_lesson/homework_9_1/Program.cs
+++ 9_lesson/homework_9_1/Program.cs
@@ -3,15 +3,24 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
-// Задайте значения M и N. Напишите программу, которая 
-// рекурсивно выведет все натуральные числа в промежутке от M до N.
+
+ int Summa(int m, int n)
+ {
+     int res = m;
+     if (m == n)
+         return 0;
+     else
+     {
+         m++;
+         res = m + Summa(m, n);
+         return res;
+     }
+ }
+
+ Console.Write("Введите число M: ");
+ int m = int.Parse(Console.ReadLine());
+ Console.Write("Введите число N: ");
+ int n = int.Parse(Console.ReadLine());
+ Console.WriteLine($"Сумма чисел = {Summa(m-1, n)} ");
 
 
-void Spacing(int M, int N)
-{
-    if (N < M) return;
-    Spacing(M, N);
-    Console.Write($"{N}, ");
-}
-
-Spacing(1, 15);
